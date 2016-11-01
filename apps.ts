37f04f9bf59a646953e5dd2ccc7841551b2c53e9
Kimberly Ames <ameskimberly@gmail.com>
2016-11-01 10:19:20 -0700

    add initial files

---
+++ apps.ts
@@ -0,0 +1,12 @@
+console.log(parseInt("10")+parseInt("10"));
+/*
+let dogName: string = "Sammy";
+let age: number = 2;
+let female: boolean = true;
+
+let bobname = "Bob";
+
+console.log(typeof(bobname));
+
+console.log("name= " + dogName + " age= " + age);
+*/
